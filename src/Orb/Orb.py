dbfd8543be84fc34e325f357e72554c872585223
Ubuntu <tully_tim@yahoo.com>
2022-11-11 00:34:44 +0000

    latest

--- src/Orb/Orb.py
+++ src/Orb/Orb.py
@@ -1,3 +1,17 @@
+# Copyright 2022 Menlo Ventures
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import datetime
 import json
 import uuid
@@ -205,7 +219,7 @@ class Orb:
         return json.loads(response.content)
 
     def __repr__(self):
-          return f"Orb(\"{self.api_key}\", {self.debug})"
+        return f"Orb(\"{self.api_key}\", {self.debug})"
 
     def __str__(self):
         BOLD = "\033[1m"
